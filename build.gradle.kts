4507b6724f8c623e9326d7a6c21c3ba9c815be8b
Mikołaj Lemański <mikolaj.lemanski@efento.com>
2025-02-26 15:14:41 +0100

    Added publishing workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.kotlinMultiplatform) apply  false
-    alias(libs.plugins.vanniktech.mavenPublish) apply false
     alias(libs.plugins.test.resources.plugin) apply false
+    alias(libs.plugins.maven.publish) apply false
 }
--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,19 +1,31 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
     alias(libs.plugins.test.resources.plugin)
-    id("maven-publish")
+    alias(libs.plugins.maven.publish)
+    signing
 }
 
 group = "io.github.xmlpullkmp"
 version = "1.0.0"
 
+android {
+    namespace = "io.github.xmlpullkmp"
+    defaultConfig {
+        compileSdk = libs.versions.android.compileSdk.get().toInt()
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+}
+
 kotlin {
     jvmToolchain(17)
 
-    jvm {
-        withJava()
+    androidTarget {
+        publishLibraryVariants("release")
     }
-
+    jvm()
     iosX64()
     iosArm64()
     iosSimulatorArm64()
@@ -54,8 +66,74 @@ tasks.withType<Test> {
     }
 }
 
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
 
+    coordinates(
+        groupId = group.toString(),
+        artifactId = "xmlpullkmp",
+        version = version.toString()
+    )
 
+    println(SonatypeHost.DEFAULT.toString())
 
+    pom {
+        name.set("XmlPullKmp")
+        description.set("Kotlin Multiplatform implementation of XmlPullParser.")
+        inceptionYear.set("2025")
+        url.set("https://github.com/XmlPullKmp/XmlPullKmp")
+
+        licenses {
+            license {
+                name.set("Apache-2.0 license")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("matekar")
+                name.set("Marcin Kiżewski")
+                email.set("kiewskimarcin@gmail.com")
+            }
+
+            developer {
+                id.set("lemcoder")
+                name.set("Mikołaj Lemański")
+                email.set("lemanski.dev@gmail.com")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/XmlPullKmp/XmlPullKmp")
+        }
+    }
+}
+
+// Sign with default plugin
+signing {
+    useInMemoryPgpKeys(
+        System.getenv("PRIVATE_GPG_KEY"),
+        System.getenv("PRIVATE_GPG_KEY_PASSWORD")
+    )
+    sign(publishing.publications)
+
+    // Temporary workaround, see https://github.com/gradle/gradle/issues/26091#issuecomment-1722947958
+    tasks.withType<AbstractPublishToMaven>().configureEach {
+        val signingTasks = tasks.withType<Sign>()
+        mustRunAfter(signingTasks)
+    }
+}
+
+// Disable iOS testing
+afterEvaluate {
+    if (System.getProperty("os.name").lowercase().contains("mac")) {
+        tasks.getByName("compileTestKotlinIosArm64").enabled = false
+        tasks.getByName("compileTestKotlinIosX64").enabled = false
+        tasks.getByName("compileTestKotlinIosSimulatorArm64").enabled = false
+        tasks.getByName("compileTestKotlinMacosArm64").enabled = false
+        tasks.getByName("compileTestKotlinMacosX64").enabled = false
+    }
+}
 
 
